24ab538eefc15e45e94206b9dbea47acfb8d8fa7
Jose Cruz <jf.cruz001@ebc.edu.mx>
2020-03-11 13:27:28 -0600

    coding master solving exercise 3 part 2

--- 2015/advent_of_code/lib/advent_of_code/exercise_3.ex
+++ 2015/advent_of_code/lib/advent_of_code/exercise_3.ex
@@ -1,29 +1,52 @@
 defmodule Advent_of_code.Houses_in_a_vacuum do
 
-    def deliver_gifts_in_grid(instructions) do
-        instructions
-        |> String.split("")
-        |> follow_instrucctions({0, 0}, [])
-        |> Enum.uniq
-        |> Enum.count
-    end
+  def deliver_gifts_in_grid(instructions) do
+    instructions
+    |> String.split("")
+    |> follow_instrucctions({0, 0}, [])
+    |> Enum.uniq
+    |> Enum.count
+  end
 
-    def follow_instrucctions([], _, steps_followed) do 
-        steps_followed
-    end
+  defp follow_instrucctions([], _, steps_followed),do: steps_followed
 
-    def follow_instrucctions([next | instructions], current_coord, steps_followed) do 
-        follow_instrucctions(instructions, next_move(next, current_coord), [current_coord | steps_followed])
-    end
+  defp follow_instrucctions([next | instructions], current_coord, steps_followed) do 
+    follow_instrucctions(instructions, next_move(next, current_coord), [current_coord | steps_followed])
+  end
 
-    def of(n), do: compute(n, 1)
-    defp compute(1, accum), do: accum
-    defp compute(n, accum), do: compute(n-1, accum * n)
+  defp next_move("^", {x, y}), do: {x, y + 1}
+  defp next_move("v", {x, y}), do: {x, y - 1}
+  defp next_move(">", {x, y}), do: {x + 1, y}
+  defp next_move("<", {x, y}), do: {x - 1, y}
+  defp next_move(_, coord), do: coord
 
-    def next_move("^", {x, y}), do: {x, y + 1}
-    def next_move("v", {x, y}), do: {x, y - 1}
-    def next_move(">", {x, y}), do: {x + 1, y}
-    def next_move("<", {x, y}), do: {x - 1, y}
-    def next_move(_, coord), do: coord
+  
+  def deliver_gifts_in_grid_with_robo_santa(instructions) do
+    instructions
+    |> String.split("", trim: true)
+    |> follow_instrucctions({0, 0}, {0, 0}, [])
+    |> Enum.uniq
+    |> Enum.count
+  end 
+
+  defp follow_instrucctions([], santas_position, robos_position, steps_followed), do: [santas_position, robos_position | steps_followed]
+
+  defp follow_instrucctions([santas_next | []], santas_position, robos_position, steps_followed) do
+    follow_instrucctions(
+      [], 
+      next_move(santas_next, santas_position), 
+      robos_position,
+      [santas_position, robos_position | steps_followed]
+    )
+  end
+
+  defp follow_instrucctions([santas_next, robos_next | instructions], santas_position, robos_position, steps_followed) do 
+    follow_instrucctions(
+      instructions, 
+      next_move(santas_next, santas_position), 
+      next_move(robos_next, robos_position),
+      [santas_position, robos_position | steps_followed]
+    )
+  end
 
 end
