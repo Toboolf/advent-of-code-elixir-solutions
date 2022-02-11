dcbc34311e56a9df80937a5f9946126bf5fba1da
Jose Cruz <floobot@hotmail.com>
2022-02-11 07:36:54 -0600

    coding master fixing test for excercise 3

--- 2015/advent_of_code/lib/advent_of_code/exercise_3.ex
+++ 2015/advent_of_code/lib/advent_of_code/exercise_3.ex
@@ -2,16 +2,17 @@ defmodule Advent_of_code.Houses_in_a_vacuum do
 
   def deliver_gifts_in_grid(instructions) do
     instructions
-    |> String.split("")
-    |> follow_instrucctions({0, 0}, [])
+    |> String.split("", trim: true)
+    |> follow_instrucctions({0, 0}, [{0, 0}])
     |> Enum.uniq
     |> Enum.count
   end
 
-  defp follow_instrucctions([], _, steps_followed),do: steps_followed
+  defp follow_instrucctions([], _, steps_followed), do: steps_followed
 
-  defp follow_instrucctions([next | instructions], current_coord, steps_followed) do 
-    follow_instrucctions(instructions, next_move(next, current_coord), [current_coord | steps_followed])
+  defp follow_instrucctions([next | instructions], current_coord, steps_followed) do
+    next_coord = next_move(next, current_coord)
+    follow_instrucctions(instructions, next_coord, [next_coord | steps_followed])
   end
 
   defp next_move("^", {x, y}), do: {x, y + 1}
@@ -20,30 +21,30 @@ defmodule Advent_of_code.Houses_in_a_vacuum do
   defp next_move("<", {x, y}), do: {x - 1, y}
   defp next_move(_, coord), do: coord
 
-  
+
   def deliver_gifts_in_grid_with_robo_santa(instructions) do
     instructions
     |> String.split("", trim: true)
     |> follow_instrucctions({0, 0}, {0, 0}, [])
     |> Enum.uniq
     |> Enum.count
-  end 
+  end
 
   defp follow_instrucctions([], santas_position, robos_position, steps_followed), do: [santas_position, robos_position | steps_followed]
 
   defp follow_instrucctions([santas_next | []], santas_position, robos_position, steps_followed) do
     follow_instrucctions(
-      [], 
-      next_move(santas_next, santas_position), 
+      [],
+      next_move(santas_next, santas_position),
       robos_position,
       [santas_position, robos_position | steps_followed]
     )
   end
 
-  defp follow_instrucctions([santas_next, robos_next | instructions], santas_position, robos_position, steps_followed) do 
+  defp follow_instrucctions([santas_next, robos_next | instructions], santas_position, robos_position, steps_followed) do
     follow_instrucctions(
-      instructions, 
-      next_move(santas_next, santas_position), 
+      instructions,
+      next_move(santas_next, santas_position),
       next_move(robos_next, robos_position),
       [santas_position, robos_position | steps_followed]
     )
--- 2015/advent_of_code/test/advent_of_code_test.exs
+++ 2015/advent_of_code/test/advent_of_code_test.exs
@@ -1,8 +1,16 @@
 defmodule AdventOfCodeTest do
   use ExUnit.Case
-  doctest AdventOfCode
+  doctest Advent_of_code.Houses_in_a_vacuum
 
-  test "greets the world" do
-    assert AdventOfCode.hello() == :world
+  test "> deliver gifts in grid" do
+    assert Advent_of_code.Houses_in_a_vacuum.deliver_gifts_in_grid(">") == 2
+  end
+
+  test ">> deliver gifts in grid" do
+    assert Advent_of_code.Houses_in_a_vacuum.deliver_gifts_in_grid(">>") == 3
+  end
+
+  test "><>< deliver gifts in grid" do
+    assert Advent_of_code.Houses_in_a_vacuum.deliver_gifts_in_grid("><><") == 2
   end
 end
